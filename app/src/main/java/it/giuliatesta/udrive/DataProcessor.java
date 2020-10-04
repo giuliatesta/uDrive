549cce8e9de7dc93707224f3972a91565dbabd77
Giulia Testa <giulia.testa01@universitadipavia.it>
2020-10-04 15:07:28 +0200

    Modifiche al Data Processor

--- app/src/main/java/it/giuliatesta/udrive/DataProcessor.java
+++ app/src/main/java/it/giuliatesta/udrive/DataProcessor.java
@@ -1,30 +1,45 @@
 package it.giuliatesta.udrive;
 
-import it.giuliatesta.udrive.accelerometer.Acceleration;
-import it.giuliatesta.udrive.accelerometer.DataEvent;
-import it.giuliatesta.udrive.accelerometer.Direction;
-
 import static java.lang.Math.*;
 import static java.lang.Math.pow;
 
 public class DataProcessor {
 
-    int MaxValueRange;
-    int MinValueRange;
+    int MaxValue;
+    int MinValue;
 
-    // Calcola il modulo del vettore accelerazione
+    /*
+        Calcola il modulo del vettore accelerazione
+     */
     public double getAccelerationVector(double x, double y, double z) {
         return sqrt(pow(x,2) + pow(y,2) + pow(z,2));
     }
 
+    /*
+        Se il vettore supera il valore massimo oppure è minore del valore minino il punteggio è zero;
+        mentre se rientra nel range viene calcolata una percentuale particolare
+     */
     public int getPercentage(double vector) {
             int perc = 0;
-            if(vector > MaxValueRange || vector < MinValueRange) {
+            if(vector > MaxValue || vector < MinValue) {
                 perc = 0;
             } else {
-                //calculatePercentage();
+                perc = calculatePercentage(vector);
             }
             return perc;
+    }
+    /*
+        Calcola una percentuale.
+     */
+    private int calculatePercentage(double vector) {
+        // Calcola il valore medio
+        double mediumValue = getMediumValue(MaxValue, MinValue);
+        // Calcola il valore dell'intervallo dei valori
+        double range = MaxValue - MinValue;
+        // rapporto tra la variazione dal valor medio e l'intervallo di valori
+        double ratio = (abs(vector - mediumValue))/range;
+        // La percentuale è il rapporto per 100
+        return (int) (ratio*100);
     }
 
     public double getMediumValue(int max, int min) {

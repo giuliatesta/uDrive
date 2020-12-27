591555d4456e7c7ea69007df866fc4989bf8bcaa
Giulia Testa <giulia.testa01@universitadipavia.it>
2020-12-27 17:42:31 +0100

    Modifiche all'AccelerometerDataEvent

--- app/src/main/java/it/giuliatesta/udrive/accelerometer/AccelerometerDataEvent.java
+++ app/src/main/java/it/giuliatesta/udrive/accelerometer/AccelerometerDataEvent.java
@@ -19,42 +19,73 @@ public class AccelerometerDataEvent implements Serializable {
     private EventType type;
 
     /**
-     * Costruttore
-     * @param direction
-     * @param percentage
-     * @param verticalMotion
+     * Costruttore di un evento completo
+     * @param direction tipo di direzione
+     * @param directionPercentage   punteggio associato alla direzione
+     * @param verticalMotion    tipo di movimento verticale
+     * @param verticalMotionPercentage  punteggio associato al movimento
+     * @param vectorValue modulo del vettore accelerazione
      */
-    public AccelerometerDataEvent(Direction direction, int percentage, VerticalMotion verticalMotion, int verticalMotionPercentage, double vectorValue) {
+    public AccelerometerDataEvent(Direction direction, int directionPercentage, VerticalMotion verticalMotion, int verticalMotionPercentage, double vectorValue) {
         this.direction = direction;
-        this.directionPercentage = percentage;
+        this.directionPercentage = directionPercentage;
         this.verticalMotion = verticalMotion;
         this.verticalMotionPercentage = verticalMotionPercentage;
         this.vectorValue = vectorValue;
         type = BOTH;
     }
 
-    public AccelerometerDataEvent(Direction direction, int percentage, VerticalMotion verticalMotion, int verticalMotionPercentage) {
+    /**
+     * Costruttore di un evento completo, senza il valore del vettore accelerazione
+     * @param direction tipo di direzione
+     * @param directionPercentage   punteggio associato alla direzione
+     * @param verticalMotion    tipo di movimento verticale
+     * @param verticalMotionPercentage  punteggio associato al movimento
+     */
+    public AccelerometerDataEvent(Direction direction, int directionPercentage, VerticalMotion verticalMotion, int verticalMotionPercentage) {
         this.direction = direction;
-        this.directionPercentage = percentage;
+        this.directionPercentage = directionPercentage;
         this.verticalMotion = verticalMotion;
         this.verticalMotionPercentage = verticalMotionPercentage;
         type = BOTH;
     }
 
+    /**
+     * Metodo per la creazione di un evento di tipo DIRECTION_EVENT
+     * @param direction     tipo di direzione
+     * @param directionPercentage   punteggio associato alla direzione
+     * @return
+     */
     public static AccelerometerDataEvent createDirectionEvent(Direction direction, int directionPercentage) {
         return new AccelerometerDataEvent(direction, directionPercentage);
     }
 
+    /**
+     * Metodo per la creazione di un evento di tipo VERTICAL_MOTION_EVENT
+     * @param verticalMotion    tipo di movimento verticale
+     * @param verticalMotionPercentage punteggio associato al movimento
+     * @return
+     */
     public static AccelerometerDataEvent createVerticalMotionEvent(VerticalMotion verticalMotion, int verticalMotionPercentage) {
         return new AccelerometerDataEvent(verticalMotion, verticalMotionPercentage);
     }
 
+    /**
+     * Costrutore per un evento di tipo DIRECTION_EVENT
+     * @param direction tipo di direzione
+     * @param directionPercentage punteggio associato alla direzione
+     */
     private AccelerometerDataEvent(Direction direction, int directionPercentage) {
         this.direction = direction;
         this.directionPercentage = directionPercentage;
         type = DIRECTION_EVENT;
     }
 
+    /**
+     * Costruttore per un evento di tipo VERTICAL_MOTION_EVENT
+     * @param verticalMotion    tipo di movimento
+     * @param verticalMotionPercentage  punteggio associato al movimento
+     */
     private AccelerometerDataEvent(VerticalMotion verticalMotion, int verticalMotionPercentage) {
         this.verticalMotion = verticalMotion;
         this.verticalMotionPercentage = verticalMotionPercentage;
@@ -63,68 +94,70 @@ public class AccelerometerDataEvent implements Serializable {
 
     /**
      * Metodo get per la direzione indicata dall'accelerometro
+     * Non dovrebbe mai accadere che viene richiesta la direction quando l'evento è ti tipo VERTICAL_MOTION_EVENT
      * @return direzione indicata dall'accelerometro
      */
-    /*
+
     public Direction getDirection() {
-        if(type == DIRECTION_EVENT) {
-            return direction;
-        } else {
+        if (type == VERTICAL_MOTION_EVENT) {
             throw new IllegalArgumentException("IMPOSSIBLE. This is a VerticalMotionEvent!");
+        } else {
+            return direction;
         }
-    }
-*/
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public int getDirectionPercentage() {
-        return directionPercentage;
-    }
-
-    public VerticalMotion getVerticalMotion() {
-        return verticalMotion;
-    }
-
-    public int getVerticalMotionPercentage() {
-        return verticalMotionPercentage;
     }
 
     /**
-     * Metodo get per il punteggio ottenuto
+     * Metodo get per il punteggio ottenuto della direzione
+     * Non dovrebbe mai accadere che viene richiesta la directionPercentage quando l'evento è ti tipo VERTICAL_MOTION_EVENT
      * @return percentuale
      */
-    /*
+
     public int getDirectionPercentage() {
-        if(type == DIRECTION_EVENT) {
-            return directionPercentage;
-        } else {
+        if (type == VERTICAL_MOTION_EVENT) {
             throw new IllegalArgumentException("IMPOSSIBLE. This is a VerticalMotionEvent!");
+        } else {
+            return directionPercentage;
         }
     }
 
+    /**
+     * Metodo get per il punteggio ottenuto del movimento verticale
+     * Non dovrebbe mai accadere che viene richiesta la verticalMotionPercentage quando l'evento è ti tipo DIRECTION_EVENT
+     * @return percentuale
+     */
     public int getVerticalMotionPercentage() {
-        if(type == VERTICAL_MOTION_EVENT) {
-            return verticalMotionPercentage;
+        if (type == DIRECTION_EVENT) {
+            throw new IllegalArgumentException("IMPOSSIBLE. This is a VerticalMotionEvent!");
         } else {
-            throw new IllegalArgumentException("IMPOSSIBLE. This is a DirectionEvent!");
+            return verticalMotionPercentage;
         }
     }
 
+    /**
+     * Metodo get per il tipo di movimento verticale
+     * Non dovrebbe mai accadere che viene richiesta la verticalMotion quando l'evento è ti tipo DIRECTION_EVENT
+     * @return movimento verticale
+     */
     public VerticalMotion getVerticalMotion() {
-        if(type == VERTICAL_MOTION_EVENT) {
-            return verticalMotion;
+        if (type == DIRECTION_EVENT) {
+            throw new IllegalArgumentException("IMPOSSIBLE. This is a VerticalMotionEvent!");
         } else {
-            throw new IllegalArgumentException("IMPOSSIBLE. This is a DirectionEvent!");
+            return verticalMotion;
         }
-    }*/
+    }
 
-
+    /**
+     * Metodo get per il tipo di evento
+     * @return tipo di evento
+     */
     public EventType getType() {
         return type;
     }
 
+    /**
+     * Metodo get per il valore del modulo dell'accelerazione
+     * @return modulo dell'accelerazione
+     */
     public double getVectorValue() {
         return vectorValue;
     }
